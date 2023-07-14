87c014ebe424f9781d3f0fb51d109ae38aa14583
sandy <sandy.ali.basha@gmail.com>
2023-07-15 00:43:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,79 @@
+
+$(document).ready(function(){
+
+
+	/* ---- Countdown timer ---- */
+
+	$('#counter').countdown({
+		timestamp : (new Date()).getTime() + 11*24*60*60*1000
+	});
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+
+	/* ---- Using Modernizr to check if the "required" and "placeholder" attributes are supported ---- */
+
+	if (!Modernizr.input.placeholder) {
+		$('.email').val('Input your e-mail address here...');
+		$('.email').focus(function() {
+			if($(this).val() == 'Input your e-mail address here...') {
+				$(this).val('');
+			}
+		});
+	}
+
+	// for detecting if the browser is Safari
+	var browser = navigator.userAgent.toLowerCase();
+
+	if(!Modernizr.input.required || (browser.indexOf("safari") != -1 && browser.indexOf("chrome") == -1)) {
+		$('form').submit(function() {
+			$('.popup').remove();
+			if(!$('.email').val() || $('.email').val() == 'Input your e-mail address here...') {
+				$('form').append('<p class="popup">Please fill out this field.</p>');
+				$('.email').focus();
+				return false;
+			}
+		});
+		$('.email').keydown(function() {
+			$('.popup').remove();
+		});
+		$('.email').blur(function() {
+			$('.popup').remove();
+		});
+	}
+	
+});
+
+//swiper js init
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+	el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+	nextEl: '.swiper-button-next',
+	prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+	el: '.swiper-scrollbar',
+  },
+});
